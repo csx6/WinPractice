54b3187e3f3233d4eb43f80ae451e9ab2038fda9
Administrator <Administrator@SKY-20170617ADF>
2022-04-27 22:47:36 +0900

    プロジェクト ファイルを追加します。

---
+++ Practice1-6-1/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Dim strTemp As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "モグラ" Then
+            strTemp = Button1.Text
+            Button1.Text = Button2.Text
+            Button2.Text = strTemp
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Button2.Text = "モグラ" Then
+            strTemp = Button1.Text
+            Button1.Text = Button2.Text
+            Button2.Text = strTemp
+        End If
+    End Sub
+End Class
